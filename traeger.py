c06646d026990930336c74cf5c0d3aaa67f0829b
Sam Atkins <sam@trufflesec.com>
2024-03-05 11:21:19 -0700

    Super simple console logging option

--- traeger.py
+++ traeger.py
@@ -7,19 +7,29 @@ and is released under the "GNU GENERAL PUBLIC LICENSE Version 2".
 Please see the LICENSE file that should have been included as part of this package.
 """
 
-import time
+import json
 import ssl
+import time
+import urllib
+import uuid
+
 import paho.mqtt.client as mqtt
 import requests
-import uuid
-import urllib
-import json
-
 
 CLIENT_ID = "2fuohjtqv1e63dckp5v84rau0j"
 
+
 class traeger:
-    def __init__(self, username, password, token = None, token_expires = 0, mqtt_url = None, mqtt_url_expires = 0, mqtt_uuid = str(uuid.uuid1())):
+    def __init__(
+        self,
+        username,
+        password,
+        token=None,
+        token_expires=0,
+        mqtt_url=None,
+        mqtt_url_expires=0,
+        mqtt_uuid=str(uuid.uuid1()),
+    ):
         self.username = username
         self.password = password
         self.token = token
@@ -37,21 +47,33 @@ class traeger:
     def refresh_token(self):
         if self.token_remaining() < 60:
             request_time = time.time()
-            r = requests.post("https://cognito-idp.us-west-2.amazonaws.com/", json = 
-                    {"AuthFlow":"USER_PASSWORD_AUTH",
-                     "AuthParameters":{"USERNAME": self.username,
-                                       "PASSWORD": self.password},
-                     "ClientId": CLIENT_ID},
-                              headers = {'Content-Type': 'application/x-amz-json-1.1',
-                                         'X-Amz-Target': 'AWSCognitoIdentityProviderService.InitiateAuth'})
+            r = requests.post(
+                "https://cognito-idp.us-west-2.amazonaws.com/",
+                json={
+                    "AuthFlow": "USER_PASSWORD_AUTH",
+                    "AuthParameters": {
+                        "USERNAME": self.username,
+                        "PASSWORD": self.password,
+                    },
+                    "ClientId": CLIENT_ID,
+                },
+                headers={
+                    "Content-Type": "application/x-amz-json-1.1",
+                    "X-Amz-Target": "AWSCognitoIdentityProviderService.InitiateAuth",
+                },
+            )
             response = r.json()
-            self.token_expires = response["AuthenticationResult"]["ExpiresIn"] + request_time
+            self.token_expires = (
+                response["AuthenticationResult"]["ExpiresIn"] + request_time
+            )
             self.token = response["AuthenticationResult"]["IdToken"]
 
     def get_user_data(self):
         self.refresh_token()
-        r = requests.get("https://1ywgyc65d1.execute-api.us-west-2.amazonaws.com/prod/users/self",
-                         headers = {'authorization': self.token})
+        r = requests.get(
+            "https://1ywgyc65d1.execute-api.us-west-2.amazonaws.com/prod/users/self",
+            headers={"authorization": self.token},
+        )
         json = r.json()
         return json
 
@@ -67,8 +89,10 @@ class traeger:
         self.refresh_token()
         if self.mqtt_url_remaining() < 60:
             mqtt_request_time = time.time()
-            r = requests.post("https://1ywgyc65d1.execute-api.us-west-2.amazonaws.com/prod/mqtt-connections",
-                              headers = {'authorization': self.token})
+            r = requests.post(
+                "https://1ywgyc65d1.execute-api.us-west-2.amazonaws.com/prod/mqtt-connections",
+                headers={"authorization": self.token},
+            )
             json = r.json()
             self.mqtt_url_expires = json["expirationSeconds"] + mqtt_request_time
             self.mqtt_url = json["signedUrl"]
@@ -77,13 +101,17 @@ class traeger:
         if self.mqtt_client == None:
             self.refresh_mqtt_url()
             mqtt_parts = urllib.parse.urlparse(self.mqtt_url)
-            self.mqtt_client = mqtt.Client(transport = "websockets")
+            self.mqtt_client = mqtt.Client(
+                mqtt.CallbackAPIVersion.VERSION2, transport="websockets"
+            )
             self.mqtt_client.on_connect = on_connect
             self.mqtt_client.on_message = on_message
             headers = {
                 "Host": "{0:s}".format(mqtt_parts.netloc),
-                }
-            self.mqtt_client.ws_set_options(path="{}?{}".format(mqtt_parts.path, mqtt_parts.query), headers=headers)
+            }
+            self.mqtt_client.ws_set_options(
+                path="{}?{}".format(mqtt_parts.path, mqtt_parts.query), headers=headers
+            )
             context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
             context.check_hostname = False
             context.verify_mode = ssl.CERT_NONE
@@ -94,21 +122,21 @@ class traeger:
 
     def grill_message(self, client, userdata, message):
         if message.topic.startswith("prod/thing/update/"):
-            grill_id = message.topic[len("prod/thing/update/"):]
+            grill_id = message.topic[len("prod/thing/update/") :]
             self.grill_status[grill_id] = json.loads(message.payload)
-        
-    def grill_connect(self, client, userdata, flags, rc):
+
+    def grill_connect(self, client, userdata, flags, rc, junk):
         pass
-    
-    def get_grill_status(self, timeout = 10):
+
+    def get_grill_status(self, timeout=10):
         client = self.get_mqtt_client(self.grill_connect, self.grill_message)
         for grill in self.grills:
             if grill["thingName"] in self.grill_status:
                 del self.grill_status[grill["thingName"]]
-            client.subscribe(("prod/thing/update/{}".format(grill["thingName"]),1))
+            client.subscribe(("prod/thing/update/{}".format(grill["thingName"]), 1))
         for grill in self.grills:
             remaining = timeout
             while not grill["thingName"] in self.grill_status and remaining > 0:
                 time.sleep(1)
-                remaining -= 1                    
+                remaining -= 1
         return self.grill_status
--- traeger2log.py
+++ traeger2log.py
@@ -9,16 +9,20 @@ This file is part of the traeger python library,
 and is released under the "GNU GENERAL PUBLIC LICENSE Version 2". 
 Please see the LICENSE file that should have been included as part of this package.
 """
-import os
 import getpass
-import pprint
-import traeger
-import numbers
 import json
-import time
+import numbers
+import os
+import pprint
 import socket
+import sys
+import time
+from datetime import datetime
+
+import traeger
 
 pp = pprint.PrettyPrinter(indent=4)
+
 
 def unpack(base, value):
     if isinstance(value, numbers.Number):
@@ -31,6 +35,7 @@ def unpack(base, value):
         return unpack_list(base, value)
     return []
 
+
 def unpack_list(base, thelist):
     result = []
     for n, v in enumerate(thelist):
@@ -38,6 +43,7 @@ def unpack_list(base, thelist):
         newbase.append(str(n))
         result.extend(unpack(newbase, v))
     return result
+
 
 def unpack_dict(base, thedict):
     result = []
@@ -50,55 +56,35 @@ def unpack_dict(base, thedict):
             result.extend(unpack(newbase, v))
     return result
 
+
 if __name__ == "__main__":
     try:
-        config = json.load(open(os.path.expanduser("~/.traeger"),"r"))
+        config = json.load(open(os.path.expanduser("config.json"), "r"))
     except (json.JSONDecodeError, FileNotFoundError):
         config = {}
     if "username" not in config:
         config["username"] = input("username:")
     if "password" not in config:
         config["password"] = getpass.getpass()
-    if "graphite_port" not in config:
-        config["graphite_port"] = input("graphite port:")
-    if "graphite_host" not in config:
-        config["graphite_host"] = input("graphite host:")
 
-    open(os.path.expanduser("~/.traeger"),"w").write(json.dumps(config))
+    open(os.path.expanduser("config.json"), "w").write(json.dumps(config))
 
+    t = traeger.traeger(config["username"], config["password"])
 
-    t = traeger.traeger(config['username'], config['password'])
-    
     while True:
-        last_collect = time.time()
+        last_collect = datetime.now()
         grills = t.get_grills()
         grills_status = t.get_grill_status()
         for grill in grills:
             if grill["thingName"] not in grills_status:
-                print ("Missing Data for {}".format(grill["thingName"]))
+                print("Missing Data for {}".format(grill["thingName"]))
 
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((config["graphite_host"], int(config["graphite_port"])))
-            for k,v in unpack_dict([], grills_status):
-                s.send("traeger.{} {} {}\r\n".format(k, v, int(last_collect)).encode())
-            s.close()
-        except Exception as e:
-            print (e)
-        next_collect = last_collect + 60
-        until_collect = next_collect - time.time()
-        if until_collect > 0:
-            print ("Sleeeping {}".format(until_collect))
-            time.sleep(until_collect)
-        else:
-            print ("Late for next collection {}".format(until_collect))
-    
-
-
-#t = traeger.traeger(input("user:"), getpass.getpass())
-#grills = t.get_grill_status()
-#for k,v in unpack_dict([], grills): 
-#    print("{} {}".format(k, v))
-
-        
-
+        print(
+            "{},{},{}".format(
+                last_collect,
+                grills_status[grill["thingName"]]["status"]["grill"],
+                grills_status[grill["thingName"]]["status"]["probe"],
+            )
+        )
+        sys.stdout.flush()
+        time.sleep(60)
